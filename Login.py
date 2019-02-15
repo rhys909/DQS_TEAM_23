565231630d1ed9a44d2bc1118339bfc40c0a7114
Charlie Webb <cjsw00@hotmail.co.uk>
2019-02-15 23:08:04 +0000

    Added support for lecturer/student differentiation and shortened code a little bit

--- Login.py
+++ Login.py
@@ -16,38 +16,36 @@ class Login(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.grid()
-        self.createLabel()
-        self.usernameBox()
-        self.passwordBox()
-        self.submitButton()
+        self.createWidgets()
         
-    def createLabel(self):
+    def createWidgets(self):
         lblIntro = Label(self, text="Login", font=("MS", 8, "bold"))
         lblIntro.grid(row=0, column=0, sticky=NE)
 
-    def usernameBox(self):
         self.username = Entry(self)
         self.username.grid(row=2, column=0, columnspan=2, sticky=W)
 
-    def passwordBox(self):
         self.passwordentry = Entry(self, show="*")
         self.passwordentry.grid(row=4, column=0, columnspan=2, sticky=W, pady=10)
 
-    def submitButton(self):
         submit = Button(self, text="Submit", command=self.validateLogin, font=("MS", 8, "bold"))
         submit.grid(row=5, column=0, sticky=W, pady=10)
 
     def validateLogin(self):
-        if self.username.get().strip() in passwords.users:
-            key = hashlib.sha1(self.passwordentry.get().encode('utf-8')).hexdigest()
-            if key == passwords.users[self.username.get().strip()]:
-                tkm.showinfo("Login Successful", "Login Successful")
+        user = self.username.get().strip()
+        pwd = self.passwordentry.get()
+        if user in passwords.users:
+            key = hashlib.sha1(pwd.encode('utf-8')).hexdigest()
+            if key == passwords.users[user][0]:
+                if passwords.users[user][1] == "Lecturer":
+                    tkm.showinfo("Login Successful", "Login Successful. You are a lecturer")
+                else:
+                    tkm.showinfo("Login Successful", "Login Successful. You are a student")
             else:
                 tkm.showinfo("Password Incorrect", "Password Incorrect")
         else:
             tkm.showinfo("No such user", "No such user")
-    
-
+            
 #Main
 root = Tk()
 root.title("Login")
--- passwords.py
+++ passwords.py
@@ -1,4 +1,4 @@
 users = {
-    "testaccount" : "226fa2c1671d250b1c155b836bb6a0f6e9ea2b03",
-    "altaccount": "f62479b6bb2cbff4708920fedbedf49f9accf8ff"
+    "testaccount" : ("226fa2c1671d250b1c155b836bb6a0f6e9ea2b03", "Lecturer"),
+    "altaccount": ("f62479b6bb2cbff4708920fedbedf49f9accf8ff", "Student"),
     }
