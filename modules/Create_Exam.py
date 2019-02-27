3490e06b882d928b68d961a74f76dc9c327f75ff
rhys909 <rhyskconnor99@gmail.com>
2019-02-27 11:22:27 +0000

    Started create exam window

---
+++ modules/Create_Exam.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class New_Exam(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.grid()
+        self.init_window()
+
+root = Tk()
+root.title("New Exam")
+app = New_Exam(root)
+root.mainloop()
--- modules/Lecturer_UI.py
+++ modules/Lecturer_UI.py
@@ -1,9 +1,10 @@
 from tkinter import *
+from modules.Create_Exam import *
 
 class Lecturer_UI(Frame):
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)               
+        Frame.__init__(self, master)
         self.master = master
         self.grid()
         self.init_window()
@@ -20,12 +21,12 @@ class Lecturer_UI(Frame):
         ]
         self.v1 = IntVar()
         position = 1
-        
+
         for text, val in openExams:
             b1 = Radiobutton(self, text=text, variable=self.v1, value=val)
             b1.grid(row=position, column=0, sticky=W)
             position += 1
-            
+
         modify = Button(self, text="modify")
         modify.grid(row=position, column=0, sticky=W)
 
@@ -33,11 +34,11 @@ class Lecturer_UI(Frame):
         dele.grid(row=position, column=1, sticky=W)
 
         position += 1
-        
+
         c_exams = Label(self, text="Your closed exams:", font=("MS", 16, "bold")).grid(row=position, column=0, sticky=W)
 
         position += 1
-        
+
         self.v2 = IntVar()
 
         formExams = [
@@ -50,11 +51,21 @@ class Lecturer_UI(Frame):
         for text, val in formExams:
             b2 = Radiobutton(self, text=text,variable=self.v2, value=val)
             b2.grid(row=position, column=0, sticky=W)
-            
+
             position += 1
 
         stats = Button(self, text="View statistics")
         stats.grid(row=position, column=0, sticky=W)
 
-        createExam = Button(self, text="Create exam")
+        def create_exam():
+            t1.TopLevel(root)
+            New_Exam(t1)
+            t1.geometry("600x500")
+            root.withdraw()
+            t1.lift()
+            t1.attributes("-topmost", True)
+            t1.resizable(False, False)
+
+
+        createExam = Button(self, text="Create exam", command = self.create_exam())
         createExam.grid(row=position, column=1, sticky=W)
