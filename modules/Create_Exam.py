6478b3d1c74c8c5eb11c6ee6c0a523cfbcd48d24
Rhys909 <rhyskconnor99@gmail.com>
2019-02-27 22:38:11 +0000

    updated with boolean test

--- modules/Create_Exam.py
+++ modules/Create_Exam.py
@@ -8,6 +8,20 @@ class New_Exam(Frame):
         self.grid()
         self.init_window()
 
+    def init_window(self):
+        self.master.title("New Test")
+        lblStrAgr = label(self, text = 'True', font=("MS", 16, "bold"))
+        lblStrAgr.grid(row=3, column= 4, rowspan=2)
+        lblStrAgr = label(self, text = 'False', font=("MS", 16, "bold"))
+        lblStrAgr.grid(row=3, column= 5, rowspan=2)
+
+
+        self.varQ1 = IntVar()
+        R1Q1 = Radiobutton(self, variable=self.varQ1, value=0)
+        R1Q1.grid(row=5, column= 4)
+        R2Q1 = Radiobutton(self, variable=self.varQ1, value=1)
+        R2Q1.grid(row=5, column= 5)
+
 root = Tk()
 root.title("New Exam")
 app = New_Exam(root)
