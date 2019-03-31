f1b6488defdee14c4e7d5f034f4409f0c4b50829
DonneyOne <43879053+DonneyOne@users.noreply.github.com>
2019-03-31 14:44:12 +0100

    Add files via upload
    Fixed bug - formative

--- exams/exams.py
+++ exams/exams.py
@@ -5,6 +5,11 @@ with open("exams/list_of_exams.csv") as file:
     exams = list(csv.reader(file))
 for i in range(len(exams)):
     summativeExams[i+1] = (exams[i][0], exams[i][1], exams[i][2])
+with open("exams/list_of_formative.csv") as file:
+	form = list(csv.reader(file))
+for i in range(len(form)):
+    formativeExams[i+1] = (exams[i][0], exams[i][1], exams[i][2])
+
     
 
 #Exams are stored in this format
