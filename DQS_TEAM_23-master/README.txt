3080d6c4ed00d4530ccda7919a8aeba934a1cbe0
Charlie Webb <cjsw00@hotmail.co.uk>
2019-04-01 14:15:13 +0100

    Fixed extra file change

--- DQS_TEAM_23-master/README.txt
+++
@@ -1,2 +0,0 @@
-# DQS_TEAM_23
-Run the main.py file if you want anything to work
--- DQS_TEAM_23-master/accounts/accounts.py
+++
@@ -1,4 +0,0 @@
-users = {
-    "testaccount" : ("226fa2c1671d250b1c155b836bb6a0f6e9ea2b03", "Lecturer"),
-    "altaccount": ("f62479b6bb2cbff4708920fedbedf49f9accf8ff", "Student"),
-    }
