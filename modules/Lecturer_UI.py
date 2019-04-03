ac8b19a75375aa713b73f9d65e2ed8a17508c58b
Bartlomiej Krol <krolba@cardiff.ac.uk>
2019-04-03 12:02:49 +0100

    view stats buttons working

--- modules/Lecturer_UI.py
+++ modules/Lecturer_UI.py
@@ -20,11 +20,6 @@ class Lecturer_UI(Frame):
         self.master.title("Lecturer")
         welcome = Label(self, text="Welcome, " + self.username + "!", font=("MS", 16, "bold")).grid(row=0, column=0, sticky=W)
         o_exams = Label(self, text="Your open exams:", font=("MS", 16, "bold")).grid(row=1, column=0, sticky=W)
-        openExams = []
-        for exam in summativeExams:
-            openExams.append((summativeExams[exam][0], exam))
-        for exam in formativeExams:
-            openExams.append((formativeExams[exam][0], exam))
         self.v1 = IntVar()
         position = 2
         with open("exams/list_of_exams.csv") as file:
@@ -39,6 +34,7 @@ class Lecturer_UI(Frame):
                     b1.grid(row=position, column=0, sticky=W)
                     position += 1
 
+
         modify = Button(self, text="modify")
         modify.grid(row=position, column=0, sticky=W)
 
@@ -48,35 +44,44 @@ class Lecturer_UI(Frame):
         position += 1
         c_exams = Label(self, text="Your closed exams:", font=("MS", 16, "bold")).grid(row=position, column=0, sticky=W)
         position += 1
-        closedExams = []
         self.v2 = IntVar()
         position += 1
-        for k in summativeExams:
-            closedExams.append((summativeExams[k][0], k))
-        for k in formativeExams:
-            closedExams.append((formativeExams[k][0], k))
        
-        for text, val in closedExams:
-            b2 = Radiobutton(self, text=text,variable=self.v2, value=val)
-            b2.grid(row=position, column=0, sticky=W)
-            position += 1
+        with open("exams/list_of_exams.csv") as file:
+            exams = list(csv.reader(file))
+            for i in range(len(exams)):
+                if exams[i][3] == "Summative":
+                    b2 = Radiobutton(self, text=exams[i][0] + "\t" + "\t" + "\t" + "Summative",variable=self.v2, value=i)
+                    b2.grid(row=position, column=0, sticky=W)
+                    position += 1
+                else:
+                    b2 = Radiobutton(self, text=exams[i][0] + "\t" + "\t" + "\t" + "Formative",variable=self.v2, value=i)
+                    b2.grid(row=position, column=0, sticky=W)
+                    position += 1
+
 
         position += 1
 
-        stats = Button(self, text="View statistics", command = self.view_statistics)
+        stats = Button(self, text="View formative statistics", command = self.view_statistics_f)
         stats.grid(row=position, column=0, sticky=W)
 
+        stats = Button(self, text="View summative statistics", command = self.view_statistics_s)
+        stats.grid(row=position, column=1, sticky=W)
+
+        position += 1
 
         createExam = Button(self, text="Create exam", command = self.create_new_exam)
-        createExam.grid(row=position, column=1, sticky=W)
+        createExam.grid(row=position, column=0, sticky=W)
 
     def create_new_exam(self):
         t1 = Toplevel()
         Create_Exam(t1)
 
-    def view_statistics(self):
+    def view_statistics_f(self):
+        ex = int(self.v2.get()) + 1
         with open("modules/passInfo.txt", "a") as viewExam:
-            viewExam.write("exams/" + formativeExams[self.v2.get()][1])
+            viewExam.write("exams/" + formativeExams[ex][1])
+
 
         t1 = Toplevel()
         Statistics(t1)
@@ -85,3 +90,15 @@ class Lecturer_UI(Frame):
         t1.attributes("-topmost", True)
         t1.resizable(False, False)
 
+    def view_statistics_s(self):
+        ex = int(self.v2.get()) + 1
+        with open("modules/passInfo.txt", "a") as viewExam:
+            viewExam.write("exams/" + summativeExams[ex][1])
+
+
+        t1 = Toplevel()
+        Statistics(t1)
+        t1.lift()
+        t1.title(summativeExams[self.v2.get()][1])
+        t1.attributes("-topmost", True)
+        t1.resizable(False, False)
